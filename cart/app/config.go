c4531d44cc2aea142ec4a9f8cf1c38f2db94438d
Cleverson Soares <cleversonsoares.zero@gmail.com>
2021-09-03 17:51:48 -0300

    common setup / service app control

---
+++ cart/app/config.go
@@ -0,0 +1,17 @@
+package app
+
+import "github.com/Ralphbaer/hash/common"
+
+// Config is the top level configuration struct for entire application
+type Config struct {
+	EnvName               string `env:"ENV_NAME"`
+	ServerAddress         string `env:"SERVER_ADDRESS"`
+	SpecURL               string `env:"SPEC_URL"`
+}
+
+// NewConfig creates a instance of Config
+func NewConfig() *Config {
+	cfg := &Config{}
+	common.SetConfigFromEnvVars(cfg)
+	return cfg
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Ralphbaer/hash
+
+go 1.14
+
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/pkg/errors v0.9.1
+)
